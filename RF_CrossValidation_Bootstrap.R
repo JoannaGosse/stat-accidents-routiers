24536fc3a0f397f34ae1aac7ca0500adda2e94cd
Joanna <109094347+JoannaGosse@users.noreply.github.com>
2025-01-08 16:24:47 +0100

    Add cross validation and bootstrap
    + Clean

--- RF_CrossValidation_Bootstrap.R
+++ RF_CrossValidation_Bootstrap.R
@@ -6,35 +6,37 @@ library(caret)
 ############################################# Chargement et préparation des données ############################################
 
 
-caracteristiques2019 = read.csv("C:/Joanna/Projet Stat/data/2019_caracteristiques.csv", sep = ";")
-usagers2019 = read.csv("C:/Joanna/Projet Stat/data/2019_usagers.csv", sep = ";")
-lieux2019 = read.csv("C:/Joanna/Projet Stat/data/2019_lieux.csv", sep = ";")
-vehicules2019 = read.csv("C:/Joanna/Projet Stat/data/2019_vehicules.csv", sep = ";")
+project_dir <- "C:/Users/gosse/Desktop/ENSG/GDS"
 
-caracteristiques2020 = read.csv("C:/Joanna/Projet Stat/data/2020_caracteristiques.csv", sep = ";")
-usagers2020 = read.csv("C:/Joanna/Projet Stat/data/2020_usagers.csv", sep = ";")
+caracteristiques2019 = read.csv(paste0(project_dir,"/data/2019_caracteristiques.csv"), sep = ";")
+usagers2019 = read.csv(paste0(project_dir,"/data/2019_usagers.csv"), sep = ";")
+lieux2019 = read.csv(paste0(project_dir,"/data/2019_lieux.csv"), sep = ";")
+vehicules2019 = read.csv(paste0(project_dir,"/data/2019_vehicules.csv"), sep = ";")
+
+caracteristiques2020 = read.csv(paste0(project_dir,"/data/2020_caracteristiques.csv"), sep = ";")
+usagers2020 = read.csv(paste0(project_dir,"/data/2020_usagers.csv"), sep = ";")
 usagers2020$id_usager <- NULL
-lieux2020 = read.csv("C:/Joanna/Projet Stat/data/2020_lieux.csv", sep = ";")
-vehicules2020 = read.csv("C:/Joanna/Projet Stat/data/2020_vehicules.csv", sep = ";")
+lieux2020 = read.csv(paste0(project_dir,"/data/2020_lieux.csv"), sep = ";")
+vehicules2020 = read.csv(paste0(project_dir,"/data/2020_vehicules.csv"), sep = ";")
 
-caracteristiques2021 = read.csv("C:/Joanna/Projet Stat/data/2021_caracteristiques.csv", sep = ";")
-usagers2021 = read.csv("C:/Joanna/Projet Stat/data/2021_usagers.csv", sep = ";")
+caracteristiques2021 = read.csv(paste0(project_dir,"/data/2021_caracteristiques.csv"), sep = ";")
+usagers2021 = read.csv(paste0(project_dir,"/data/2021_usagers.csv"), sep = ";")
 usagers2021$id_usager <- NULL
-lieux2021 = read.csv("C:/Joanna/Projet Stat/data/2021_lieux.csv", sep = ";")
-vehicules2021 = read.csv("C:/Joanna/Projet Stat/data/2021_vehicules.csv", sep = ";")
+lieux2021 = read.csv(paste0(project_dir,"/data/2021_lieux.csv"), sep = ";")
+vehicules2021 = read.csv(paste0(project_dir,"/data/2021_vehicules.csv"), sep = ";")
 
-caracteristiques2022 = read.csv("C:/Joanna/Projet Stat/data/2022_caracteristiques.csv", sep = ";")
+caracteristiques2022 = read.csv(paste0(project_dir,"/data/2022_caracteristiques.csv"), sep = ";")
 names(caracteristiques2022)[names(caracteristiques2022) == "Accident_Id"] <- "Num_Acc"
-usagers2022 = read.csv("C:/Joanna/Projet Stat/data/2022_usagers.csv", sep = ";")
+usagers2022 = read.csv(paste0(project_dir,"/data/2022_usagers.csv"), sep = ";")
 usagers2022$id_usager <- NULL
-lieux2022 = read.csv("C:/Joanna/Projet Stat/data/2022_lieux.csv", sep = ";")
-vehicules2022 = read.csv("C:/Joanna/Projet Stat/data/2022_vehicules.csv", sep = ";")
+lieux2022 = read.csv(paste0(project_dir,"/data/2022_lieux.csv"), sep = ";")
+vehicules2022 = read.csv(paste0(project_dir,"/data/2022_vehicules.csv"), sep = ";")
 
-caracteristiques2023 = read.csv("C:/Joanna/Projet Stat/data/2023_caracteristiques.csv", sep = ";")
-usagers2023 = read.csv("C:/Joanna/Projet Stat/data/2023_usagers.csv", sep = ";")
+caracteristiques2023 = read.csv(paste0(project_dir,"/data/2023_caracteristiques.csv"), sep = ";")
+usagers2023 = read.csv(paste0(project_dir,"/data/2023_usagers.csv"), sep = ";")
 usagers2023$id_usager <- NULL
-lieux2023 = read.csv("C:/Joanna/Projet Stat/data/2023_lieux.csv", sep = ";")
-vehicules2023 = read.csv("C:/Joanna/Projet Stat/data/2023_vehicules.csv", sep = ";")
+lieux2023 = read.csv(paste0(project_dir,"/data/2023_lieux.csv"), sep = ";")
+vehicules2023 = read.csv(paste0(project_dir,"/data/2023_vehicules.csv"), sep = ";")
 
 usagers = rbind(usagers2019, usagers2020, usagers2021, usagers2022, usagers2023)
 usagers = na.omit(usagers)
@@ -47,7 +49,6 @@ lieux = na.omit(lieux)
 
 vehicules = rbind(vehicules2019, vehicules2020, vehicules2021, vehicules2022, vehicules2023)
 vehicules = na.omit(vehicules)
-
 
 
 year_summary <- function(usagers, vehicules, lieux, caracteristiques){
@@ -103,79 +104,128 @@ year_summary_2023 = year_summary(usagers2023, vehicules2023, lieux2023, caracter
 all_year_summary = rbind(year_summary_2019, year_summary_2020, year_summary_2021, year_summary_2022, year_summary_2023)
 
 
-all_year_summary[,"grav"] <- NULL
+year_summary_2020[, "grav"] <- NULL
+year_summary_2022[, "grav"] <- NULL
+year_summary_2020 <- year_summary_2020[ , -1]
+year_summary_2022 <- year_summary_2022[ , -1]
 
 
-############################################# MODELE LINEAIRE LM? LOGIT ET GLM ############################################
-
-model1_result = lm(formula = mortel ~ place + catu + sexe + an_nais + trajet + secu1 + secu2 + secu3 + locp + actp + etatp, data = all_year_summary)
-summary(model1_result)
-
-
-#GLM
-logit_model = glm(formula = mortel ~ place + catu + sexe + an_nais + trajet + secu1 + secu2 + secu3 + locp + actp + etatp, data = all_year_summary, family = binomial(link = 'logit'))
-
-logit2_result = glm(formula = mortel ~ 1, data = all_year_summary, family = binomial(link = 'logit'))
-
-
-McFadden = 1 - logLik(logit_model)/logLik(logit2_result)
-
-
-#CLASSIFICATION ET ACCURACY AVEC LOGIT MODEL
-
-accuracy<- function(model){
-  length(which(ifelse(fitted(model)>0.5,1,0)==all_year_summary$mortel))/length(fitted(model))
-}
-
-
-accuracy(logit_model)
-
-f1_score <- function(model,threshold) {
-  # Prévisions du modèle
-  predictions_prob <- predict(model, type = "response")
-  predictions_class <- ifelse(predictions_prob > threshold, 1, 0)
-  
-  # Labels réels
-  actual_labels <- model$data$indicatrice_grav
-  
-  # Créer la matrice de confusion
-  confusion_mat <- table(Predicted = predictions_class, Actual = actual_labels)
-
-  # Calculer la précision et le rappel
-  true_positives <- confusion_mat[1, 1]
-  false_positives <- confusion_mat[1, 2]
-  false_negatives <- confusion_mat[2, 1]
-  
-  precision <- true_positives / (true_positives + false_positives)
-  recall <- true_positives / (true_positives + false_negatives)
-  
-  # Calculer le F1 score
-  f1 <- 2 * (precision * recall) / (precision + recall)
-  
-  return(f1)
-}
-
-
-fscore = f1_score(logit_model)
-
-thetas = seq(0.2,0.4,0.01)
-scores = sapply(thetas,function(theta){f1_score(logit_model,theta)})
-
-plot(thetas,scores)
-
-#analyse en composantes principales / matrice de correlation / gwr / balanced accuracy tp6 correction + f1
-
+all_year_summary[, "grav"] <- NULL
+all_year_summary <- all_year_summary[ , -1]
 
 
 
 ############################################# RANDOM FOREST ############################################
 
+#Sans équilibrage des données
+rf <- function(year_summary) {
+  # Sélectionner aléatoirement les indices pour le train et le test
+  set.seed(123) # Pour répétabilité
+  
+  # Créer les ensembles d'entraînement et de test en faisant en sorte que la proportion de mort 
+  # ou non soit équitablement répartie (ex: environ +/-75% des accidents mortels dans train_data et +/-25% dans 
+  # test_data en fonction de la taille de l'ensemble, pareil pour les accidents non mortels)
+  indices <- createDataPartition(year_summary$mortel, p = 0.75, list = FALSE)
+  train_data <- year_summary[indices, ]
+  test_data <- year_summary[-indices, ]
+  
+  
+  model = randomForest(mortel  ~ ., data = train_data, ntree = 500, do.trace = 100, maxdepth = 5, type = "classification", importance = TRUE, seed = 123)
+  varImpPlot(model)
+  predictions_model <- predict(model, newdata = test_data, type = "class")
+  
+  
+  # Préparer les prédictions et les vérités terrain
+  predictions <- ifelse(predictions_model >= 0.5, 1, 0)
+  true_values <- test_data$mortel
+  
+  levels_unique <- unique(c(predictions, true_values))
+  
+  predictions_factor <- factor(predictions, levels = levels_unique)
+  true_values_factor <- factor(true_values, levels = levels_unique)
+  
+  
+  # Créer la matrice de confusion
+  confusion_matrix <- confusionMatrix(data = predictions_factor, reference = true_values_factor)
+  
+  # Afficher la matrice de confusion et les statistiques de performance
+  print(confusion_matrix)
+  
+  f1_score <- confusion_matrix$byClass["F1"]
+  print(f1_score)
+  
+  return()
+}
 
-# Sélectionner aléatoirement les indices pour le train et le test
+#Avec équilibrage des données
+
+balanced_rf <- function(year_summary){
+  set.seed(123) # Pour répétabilité
+  
+  mortel_1_indices <- sample(which(all_year_summary$mortel == 1), sum(year_summary$mortel == 1, na.rm = TRUE))
+  mortel_0_indices <- sample(which(all_year_summary$mortel == 0), sum(year_summary$mortel == 1, na.rm = TRUE))
+  
+  # Combiner les indices
+  all_indices <- c(mortel_1_indices, mortel_0_indices)
+  
+  # Sélectionner les lignes correspondantes
+  selected_data <- all_year_summary %>% slice(all_indices)
+  
+  # Partitionner les données en 75% pour l'entraînement et 25% pour le test
+  train_indices <- sample(nrow(selected_data), 0.75 * nrow(selected_data))
+  train_set <- selected_data %>% slice(train_indices)
+  test_set <- selected_data %>% slice(-train_indices)
+  
+  
+  model = randomForest(mortel  ~ ., data = train_set, ntree = 500, do.trace = 100, maxdepth = 5, type = "classification", importance = TRUE, seed = 123)
+  varImpPlot(model)
+  predictions_model <- predict(model, newdata = test_set, type = "class")
+  
+  
+  # Préparer les prédictions et les vérités terrain
+  predictions <- ifelse(predictions_model >= 0.5, 1, 0)
+  true_values <- test_set$mortel
+  
+  levels_unique <- unique(c(predictions, true_values))
+  
+  predictions_factor <- factor(predictions, levels = levels_unique)
+  true_values_factor <- factor(true_values, levels = levels_unique)
+  
+  
+  # Créer la matrice de confusion
+  confusion_matrix <- confusionMatrix(data = predictions_factor, reference = true_values_factor)
+  
+  # Afficher la matrice de confusion et les statistiques de performance
+  print(confusion_matrix)
+  
+  f1_score <- confusion_matrix$byClass["F1"]
+  print(f1_score)
+  return()
+  
+}
+############################################# RANDOM FOREST années 2020 et 2022 séparées ############################################
+
+# Non-balanced dataset for 2020
+rf(year_summary_2020)
+
+# Balanced dataset for 2020
+balanced_rf(year_summary_2020)
+
+# Balanced dataset for 2022
+balanced_rf(year_summary_2022)
+
+
+############################################# RANDOM FOREST 2019 - 2023 ############################################
+
+balanced_rf(all_year_summary)
+
+############################################# CROSS VALIDATION et BOOTSTRAP ############################################
+
 set.seed(123) # Pour répétabilité
+mortel_1_indices <- sample(which(all_year_summary$mortel == 1), sum(all_year_summary$mortel == 1, na.rm = TRUE))
+mortel_0_indices <- sample(which(all_year_summary$mortel == 0), sum(all_year_summary$mortel == 1, na.rm = TRUE))
 
-mortel_1_indices <- sample(which(all_year_summary$mortel == 1), 9584)
-mortel_0_indices <- sample(which(all_year_summary$mortel == 0), 9584)
+#                                            ####CROSS VALIDATION####
 
 # Combiner les indices
 all_indices <- c(mortel_1_indices, mortel_0_indices)
@@ -183,74 +233,21 @@ all_indices <- c(mortel_1_indices, mortel_0_indices)
 # Sélectionner les lignes correspondantes
 selected_data <- all_year_summary %>% slice(all_indices)
 
-# Partitionner les données en 75% pour l'entraînement et 25% pour le test
-train_indices <- sample(nrow(selected_data), 0.75 * nrow(selected_data))
-train_set <- selected_data %>% slice(train_indices)
-test_set <- selected_data %>% slice(-train_indices)
-
-
-
-
-"train_indices <- createDataPartition(all_year_summary$mortel, p = 0.75, list = FALSE)
-
-# Créer les sous-ensembles en respectant la distribution
-train_set <- all_year_summary %>% slice(train_indices)
-test_set <- all_year_summary %>% slice(-train_indices)"
-
-model = randomForest(mortel  ~ ., data = train_set, ntree = 500, do.trace = 100, maxdepth = 5, type = "classification")
-
-predictions_model <- predict(model, newdata = test_set, type = "class")
-
-
-
-
-# Préparer les prédictions et les vérités terrain
-predictions <- ifelse(predictions_model >= 0.5, 1, 0)
-true_values <- test_set$mortel
-
-levels_unique <- unique(c(predictions, true_values))
-
-predictions_factor <- factor(predictions, levels = levels_unique)
-true_values_factor <- factor(true_values, levels = levels_unique)
-
-
-# Créer la matrice de confusion
-confusion_matrix <- confusionMatrix(data = predictions_factor, reference = true_values_factor)
-
-# Afficher la matrice de confusion et les statistiques de performance
-print(confusion_matrix)
-
-f1_score <- confusion_matrix$byClass["F1"]
-print(f1_score)
-
-
-
-
-
-
-############################################# CROSS VALIDATION ############################################
-
-
-# Réduire le nombre de données pour avoir un nombre de lignes équivalent entre les accidents mortels et non mortels
-set.seed(123)
-
-mortel_1_indices <- sample(which(all_year_summary$mortel == 1), 9584)
-mortel_0_indices <- sample(which(all_year_summary$mortel == 0), 9584)
-all_indices <- c(mortel_1_indices, mortel_0_indices)
-selected_data <- all_year_summary %>% slice(all_indices)
 
 # Partition des données pour validation croisée
 selected_data <- selected_data %>% sample_n(nrow(selected_data))
-n_rows_per_df <- floor(nrow(selected_data) / 4)
+n_rows_per_df <- floor(nrow(selected_data) / 8)
 
 # Répartition explicite en 4 groupes égaux
 selected_data_list <- split(selected_data, 
-                            rep(1:4, each = n_rows_per_df, length.out = nrow(selected_data)))
+                            rep(1:8, each = n_rows_per_df, length.out = nrow(selected_data)))
 
-lst_cm = list()
-for (i in 1:4) {
+accuracies <- numeric(8)
+f1score <- numeric(8)
+
+for (i in 1:8) {
   # Sélectionner les ensembles de formation et de test
-  training_indices <- c(1:4)[-i]
+  training_indices <- c(1:8)[-i]
   training_data <- do.call(rbind, selected_data_list[training_indices])
   test_data <- selected_data_list[[i]]
   
@@ -258,7 +255,7 @@ for (i in 1:4) {
   training_data$mortel <- factor(training_data$mortel, levels = c(0, 1))
   
   # Entraîner le modèle randomForest
-  model = randomForest(mortel ~ ., data = training_data, ntree = 100, do.trace = 25, maxdepth = 5, type = "classification")
+  model = randomForest(mortel ~ ., data = training_data, ntree = 500, do.trace = 100, maxdepth = 5, type = "classification")
   
   # Effectuer les prédictions
   predictions_model <- predict(model, newdata = test_data, type = "class")
@@ -277,43 +274,34 @@ for (i in 1:4) {
   print(confusion_matrix)
   
   # Stocker la matrice de confusion dans la liste
-  lst_cm = c(lst_cm, confusion_matrix)
+  f1score[i] <- confusion_matrix$byClass["F1"]
+  accuracies[i] <- confusion_matrix$overall['Accuracy']
 }
 
 
 
 
 # Afficher les résultats de la cross-validation
-print(model)
-
-# Afficher les scores de précision pour chaque pli
-print(model$resample)
 
 # Calculer la précision moyenne et l'écart-type
-mean_accuracy <- mean(model$resample$Accuracy)
-sd_accuracy <- sd(model$resample$Accuracy)
+mean_accuracy <- mean(accuracies)
+sd_accuracy <- sd(accuracies)
+
+mean_fscore <- mean(f1score)
+sd_fscore <- sd(f1score)
+
 print(paste("Précision moyenne:", mean_accuracy))
 print(paste("Écart-type de la précision:", sd_accuracy))
 
+print(paste("F1 score moyen:", mean_fscore))
+print(paste("Écart-type du f1score:", sd_fscore))
 
 
-############################################# BOOTSTRAP ############################################
 
-
-
-"   BOOTSTRAP 
-set.seed(123) # Pour répétabilité
-mortel_1_indices <- sample(which(all_year_summary$mortel == 1), 9584)
-mortel_0_indices <- sample(which(all_year_summary$mortel == 0), 9584)
-
-# Combiner les indices
-all_indices <- c(mortel_1_indices, mortel_0_indices)
-
-# Sélectionner les lignes correspondantes
-selected_data <- all_year_summary %>% slice(all_indices)
+#                                            ####BOOTSTRAP####
 
 # Nombre de répétitions pour le bootstrapping
-n_bootstraps <- 100
+n_bootstraps <- 25
 
 # Initialiser les vecteurs pour stocker les résultats
 f1_scores <- rep(NA, n_bootstraps)
@@ -354,7 +342,7 @@ for (i in 1:n_bootstraps) {
 print(mean(f1_scores)) # Moyenne des F1-scores
 print(sd(f1_scores))   # Écart-type des F1-scores
 print(quantile(f1_scores)) # Quantiles des F1-scores
-"
+
 
 
 
